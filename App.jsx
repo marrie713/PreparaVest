454e18e8c7da660712c24f0fa39585c12a2753cc
Marrie713 <marielybragil@gmail.com>
2024-04-16 13:28:17 -0300

    Co-authored-by: ketlinjardim97@gmail.com <ketlinjardim97@gmail.com> Co-authored-by: eduardoSantoss <santosscy@users.noreply.github.com> Co-authored-by: LETICIA PIRES NOGUEIRA <leticia.nogueira3@portalsesisp.org.br>

---
+++ App.jsx
@@ -0,0 +1,11 @@
+import Footer from './components/footer';
+import Cabeçalho from './components/header';
+
+export default function App(){
+  return(
+    <Footer/>
+    
+  );
+}
+
+
---
+++ components/footer/index.jsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+
+
+export default function Footer(){
+    return(
+        <View>
+            <Text>Teste</Text>
+        </View>
+    );
+}
